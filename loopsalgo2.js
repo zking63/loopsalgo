827e84413f935f7fc9a297741dc1943c23047784
zking63 <zoemking63@gmail.com>
2021-01-24 13:09:29 -0500

    second

--- loopsalgo2.js
+++ loopsalgo2.js
@@ -1,10 +1,9 @@
 for(var j = 1; j <= 5; j++){
     if (j == 1) {
         var sum1 = j;
-        console.log("Num: " + j + " Sum: " + sum1);
     } 
     else if (j > 1) {
         sum1 = j + sum1;
-        console.log("Num: " + j + " Sum: " + sum1);
     }
+    console.log("Num: " + j + " Sum: " + sum1);
 }
